7aafa6e5a1ac7b56d08db24be0dae17f4028e816
Roberto Moreno <rmoreno@kometsales.com>
2023-05-22 19:48:39 -0500

    cambios

---
+++ informe.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link de boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- link de estilo de encabezado -->
+    <link rel="stylesheet" href="./css/header.css">
+    <title>Control De Acceso|Informe</title>
+</head>
+<body>
+    
+    <!-- Inicio de encabezado -->
+        <header>
+            <?php include './componentes/header.php' ?>
+        </header>
+    <!-- Fin de encabezado -->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
