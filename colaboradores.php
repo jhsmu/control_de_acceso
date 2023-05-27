982932a35f582321cad48244c2b387e872d28877
Roberto Moreno <rmoreno@kometsales.com>
2023-05-27 13:12:03 -0500

    cambios(27/05)

--- colaboradores.php
+++ colaboradores.php
@@ -17,6 +17,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <!-- link de estilo de encabezado -->
     <link rel="stylesheet" href="./css/header.css">
+    <!-- link estilos de caja y modal-->
+    <link rel="stylesheet" href="./css/caja.css">
     <title>Control De Acceso|Colaboradores</title>
 </head>
 <body>
@@ -26,8 +28,20 @@
             <?php include './componentes/header.php' ?>
         </header>
     <!-- Fin de encabezado -->
-    <!-- Inicio de tabla -->
+   <div class="caja" >
+        <div class="posicion">
+            <select name="cargo" id="">
+                <option value="">Seleccione</option>
+                <option value="Administrativos" name="administrativo" >Administrativos</option>
+                <option value="Profesores" name="profesor" >Profesores</option>
+            </select>
+        </div>
+        <div class="posiciom">
+            <button type="submit" name="agregar" data-bs-toggle="modal" data-bs-target="#agregar_colaboradores"> Agregar Colaborador</button>
+        </div>
+   </div>
     <div class="container">
+         <!-- Inicio de tabla -->
         <table class="table">
             <thead>
                 <tr>
@@ -60,6 +74,56 @@
             ?>
             </tbody>
         </table>
+         <!-- fin de tabla -->
+    </div>
+    <!-- contenedor  -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="agregar_colaboradores" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Agregar Colaborador</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="contenido">
+                <form action="">
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="">Nombres</label>
+                            <input type="text" name="" id="">
+                        </div>
+                        <div class="col-6">
+                            <label for="">Apellidos</label>
+                            <input type="text" name="" id="">
+                        </div>
+                        <div class="col-12 mt-3">
+                            <label for="">Numero de documento</label>
+                            <input type="text" name="" id="">
+                        </div>
+                        <div class="col-6 mt-3">
+                            <label for="">Cargo</label>
+                            <select name="" id="">
+                                <option value="">Seleccione</option>
+                                <option value="">Administrativo</option>
+                                <option value="">Profesor</option>
+                            </select>
+                        </div>
+                        <div class="col-6 mt-3">
+                            <label for="">Telefono</label>
+                            <input type="text" name="" id="">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
+            <button type="button" class="btn btn-primary">Agregar</button>
+        </div>
+        </div>
+    </div>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
