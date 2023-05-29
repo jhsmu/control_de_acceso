04d5975c78c47227845969f55fcc6d7361f37d48
Roberto Moreno <rmoreno@kometsales.com>
2023-05-28 20:03:24 -0500

    nuevos cambios

--- colaboradores.php
+++ colaboradores.php
@@ -62,8 +62,13 @@
             <button type="submit" hidden id="buton"></button>
         </form>
         </div>
-        <div class="posiciom">
-            <button type="submit" name="agregar" data-bs-toggle="modal" data-bs-target="#agregar_colaboradores"> Agregar Colaborador</button>
+        <div class="todo">
+            <div class="posiciom">
+                <button type="submit" name="agregar" data-bs-toggle="modal" data-bs-target="#agregar_colaboradores"> Agregar Colaborador</button>
+            </div>
+            <div class="posiciom1">
+                <button type="submit" name="agregar" data-bs-toggle="modal" data-bs-target="#generar_qr"> Generar Codigo QR</button>
+            </div>
         </div>
    </div>
     <div class="container">
@@ -157,7 +162,38 @@
         </div>
     </div>
     </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <!-- fin del modal agregar colaborador -->
+
+    <!-- Inicio de modal Generar Codigo QR -->
+    <div class="modal" id="generar_qr" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Generar Codigo</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <label for="cargo">Cargo</label>
+            <select name="cargo" id="cargo">
+                <option value="" selected>Seleccione</option>
+                    <?php
+                        foreach ($cargos as $key => $cargo){     
+                    ?>
+                <option value="<?php echo $cargo["id_cargo"]; ?>"><?php echo $cargo["nombre"]; ?></option>
+                    <?php
+                        }                
+                        ?>
+                </select>        
+
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Generar</button>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php
--- prueba.php
+++ prueba.php
@@ -1,41 +1,22 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Carné con código QR</title>
-    <style>
-        .carne {
-            width: 300px;
-            height: 200px;
-            border: 1px solid #000;
-            padding: 20px;
-            text-align: center;
-            font-family: Arial, sans-serif;
-        }
-        .qr-code {
-            margin-top: 20px;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/prueba.css">
+    <title>carnes</title>
 </head>
 <body>
-    <div class="carne">
-        <h2>Nombre: Juan Pérez</h2>
-        <p>Cargo: Estudiante</p>
-        <div id="qr-code" class="qr-code"></div>
-        <button onclick="generarQR()">Generar código QR</button>
+    <div class="marco">
+        <div class="carnet">
+            <div class="posicion">
+            <img src="./img/prueba.png" alt="">
+            </div>
+                <p class="parrafo">Jhontana</p>
+                <p class="parrafo1">1099678231</p>
+                <p class="parrafo2">Ing Sistemas</p>
+        </div>
     </div>
-
-    <script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
-    <script>
-        function generarQR() {
-            var qrCodeDiv = document.getElementById("qr-code");
-            var qrCode = new QRCode(qrCodeDiv, {
-                width: 128,
-                height: 128
-            });
-
-            var url = "https://www.youtube.com"; // URL a codificar en el código QR
-            qrCode.makeCode(url);
-        }
-    </script>
 </body>
 </html>
