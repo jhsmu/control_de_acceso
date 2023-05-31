b59ea976cad8b02ae204ec2349f2ada844013c59
juandapope <55005861+juandapope@users.noreply.github.com>
2023-05-30 21:29:37 -0500

    lo que quiera

--- ingreso/ingreso.php
+++ ingreso/ingreso.php
@@ -10,38 +10,6 @@
         $estadoIngreso = $_POST['estado'];
 
 
-        // $consultar = "SELECT * FROM estudiante WHERE identificacion = $identificacion";
-        // $consulta2 = $DB_con->prepare($consultar);
-        // $consulta2->execute();
-        // $ingresosE = $consulta2->fetchAll(PDO::FETCH_ASSOC);
-
-        // $consulta = "SELECT * FROM colaboradores WHERE documento = $identificacion";
-        // $consulta1 = $DB_con->prepare($consulta);
-        // $consulta1->execute();
-        // $ingresosC = $consulta1->fetchAll(PDO::FETCH_ASSOC);
-
-        // foreach($ingresosC as $key => $ingresoC){
-        //     if($estado == $ingresoC['id_estado']){
-        //         $agregar = "INSERT INTO ingreso(id_colaboradores,fechaingreso) VALUES (:identificacion, :fecha_ingreso)";
-        //         $stmt = $DB_con->prepare($agregar);
-        //         $stmt->bindParam(':identificacion', $identificacion);
-        //         $stmt->bindParam(':fecha_ingreso', $fechaIngreso);
-        //         $stmt->execute();
-
-        //         $actu = "UPDATE colaboradores SET id_estado WHERE id=:identificacion'";
-        //         $cambio->bindParam('1', $estado);
-
-        //     }
-        // }
-        // if ($cambio->execute()) {
-        //     session_start();
-        //     $_SESSION['error'] = 'registro';
-        //     header("location: ../index.php");
-        //     } else {
-        //         session_start();
-        //         $_SESSION['error_1'] = 'registro';
-        //         header("location: ../index.php");
-        //     }
 
         $consultar = "SELECT * FROM colaboradores WHERE documento = $identificacion";
         $consulta2 = $DB_con->prepare($consultar);
